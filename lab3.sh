39bab7a07439ffa4d9220b79815e5c032e2ef9b8
Thomas N Tomei <thomas@royalsflush.com>
2025-08-11 23:23:07 +0100

    Lab for the third module

---
+++ lab3.sh
@@ -0,0 +1,9 @@
+# Exercise 1:
+$ mkdir /tmp/files/{pictures,photos,videos} -p
+
+# Exercise 2:
+$ cp /etc/[abc]* /tmp/files/
+
+# Exercise 3:
+$ mv /tmp/files/[ab]* /tmp/files/photos/
+$ mv /tmp/files/[c]* /tmp/files/videos/
